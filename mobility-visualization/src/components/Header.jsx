439c2b4999e6c5f7ff8e7ac4961540885597bedc
Tim <timrus99@gmail.com>
2021-11-13 14:27:56 +0100

    Changed header

--- mobility-visualization/src/components/Header.jsx
+++ mobility-visualization/src/components/Header.jsx
@@ -18,7 +18,7 @@ function Header() {
 			</a>
 		</div>
 
-		<div className="block">
+		<div className="block lg:hidden">
             <div className={isActive ? 'app' : null}>
 			<button id="nav-toggle" onClick={handleToggle} className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white">
 				<svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
