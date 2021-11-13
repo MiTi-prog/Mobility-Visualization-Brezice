e84c706db1dde484c3b05e6b706e021a152f4a53
Tim <timrus99@gmail.com>
2021-11-13 15:15:03 +0100

    Added models folder

--- mobility-visualization/src/components/Header.jsx
+++ mobility-visualization/src/components/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
         <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6 fixed w-full z-10 top-0">
 		<div className="flex items-center flex-shrink-0 text-white mr-6">
 			<a className="text-white no-underline hover:text-white hover:no-underline" href="#">
-				<span className="text-2xl pl-2"><i className="em em-grinning"></i> Brežice</span>
+				<span className="text-2xl pl-2"><i className="em em-grinning"></i> Brežice </span>
 			</a>
 		</div>
 
