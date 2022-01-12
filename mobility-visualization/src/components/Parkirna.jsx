449775abb5da7be098da484cbd3aef63559d8e37
Tim <timrus99@gmail.com>
2022-01-12 22:40:24 +0100

    Added filter for drawings

--- mobility-visualization/src/components/Parkirna.jsx
+++ mobility-visualization/src/components/Parkirna.jsx
@@ -3,7 +3,7 @@ import ReactMapboxGl from "react-mapbox-gl";
 import DrawControl from "react-mapbox-gl-draw";
 import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
 
-import parking  from "../data/parkirisca.js";
+import parking from "../data/parkirisca.js";
 
 const Map = ReactMapboxGl({
   accessToken:
@@ -12,23 +12,36 @@ const Map = ReactMapboxGl({
 
 function Parkirna() {
     let drawControl;
+    
 
   const setupGeometries = (ref) => {
     drawControl = ref;
-    parking.forEach((parkPlac) => {
-      drawControl.draw.add(parkPlac);
+    parking.map((parkPlac) => {
+        drawControl.draw.add(parkPlac); 
+      
     });
   };
 
+    const navControlStyle = {
+        right: 50,
+        top: 200
+    };
+
+    const fullscreenControlStyle= {
+        right: 50,
+        top: 150
+    };
+
   return (
     <div>
+        
       <Map
         style="mapbox://styles/mapbox/light-v10" // eslint-disable-line
         containerStyle={{
           height: "100vh",
           width: "100vw"
         }}
-        zoom={[15.3]}
+        zoom={[15.5]}
         center={[15.595651854337284, 45.90493389262271]}
       >
         <DrawControl ref={setupGeometries} />
--- mobility-visualization/src/components/ParkirnaMesta.jsx
+++ mobility-visualization/src/components/ParkirnaMesta.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react';
 import parkirisca from '../data/lokacije_parkirnih_mest.json';
 import Header from './Header';
+import Parkirna from './Parkirna';
 import ReactMapGL, {
      Marker, 
      Popup, 
@@ -9,77 +10,13 @@ import ReactMapGL, {
      FullscreenControl,
      GeoJSONLayer 
 } from 'react-map-gl';
-
-import DrawControl from "react-mapbox-gl-draw";
-import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
+import { useHistory } from 'react-router-dom';
 
 ReactMapGL.accessToken = 'pk.eyJ1IjoibWl0aTIxIiwiYSI6ImNrdzNoamxwdTFka2syb3JvdWRhM3EwNW8ifQ.OV5IlhtvWXgW2SwJbi_xYw';
 
 
 function ParkirnaMesta() {
-   {/*
-    onDrawCreate = ({ features }) => {
-        console.log(features);
-    };
-    
-    onDrawUpdate = ({ features }) => {
-        console.log({ features });
-    };
-    */}
-
-    {/*
-    const geojson = {
-        type: "FeatureCollection",
-        features: [
-        {
-            type: "Feature",
-            properties: {
-            text: "Fort Greene"
-        },
-        geometry: {
-            type: "Polygon",
-            coordinates: [
-                [
-                [-73.97777080535889, 40.69336192556367],
-                [-73.97704124450682, 40.68986390865585],
-                [-73.97315740585327, 40.68970120572578],
-                [-73.97388696670532, 40.69323177008439],
-                [-73.97777080535889, 40.69336192556367],
-                ]
-            ]
-        }
-        }
-        ]
-    };
-    
-    const geojsonStyles = {
-        lineLayout: {
-            "line-join": "round",
-            "line-cap": "round"
-        },
-        linePaint: {
-            "line-color": "#ff11ff",
-            "line-width": 4,
-            "line-opacity": 1
-        },
-        symbolLayout: {
-            "text-field": "{text}",
-            "symbol-placement": "line",
-            "text-rotation-alignment": "map",
-            "text-size": {
-              base: 1,
-              stops: [[9, 9], [14, 12]]
-            }
-        },
-        symbolPaint: {
-            "text-color": "rgba(0, 0, 0, 1)",
-            "text-halo-color": "rgba(255, 255, 255, 1)",
-            "text-halo-width": 2
-        }
-    };
-
-    */}
-    
+    const history = useHistory();
     const [long, SetLong] = useState(15.594459);
     const [lat, SetLat] = useState(45.9047747);
     //const [center, setCenter] = useState([long, lat]);
@@ -136,105 +73,113 @@ function ParkirnaMesta() {
       window.addEventListener("keydown", listener)
     }, []);
 
+    const handleOnChange = () => {
+        setIsChecked(!isChecked);
+    };
 
-    {/*
-    return (
-        <div className="App">
-            <ReactMapGL
-                style="mapbox://styles/mapbox/light-v10" // eslint-disable-line
-                containerStyle={{
-                    height: "100vh",
-                    width: "100vw"
-                }}
-                zoom={[16]}
-                center={[-73.9757752418518, 40.69144210646147]}
-            >
-               {/* <DrawControl
-                    position="top-left"
-                    onDrawCreate={this.onDrawCreate}
-                    onDrawUpdate={this.onDrawUpdate}
-                />
-                {/*<GeoJSONLayer {...geojsonStyles} data={geojson} />
-                }
-            </ReactMapGL>
-        </div>
-    );
-
-    */}
+    const [isMarker, setIsMarker] = useState(true);
+    const [isDraw, setIsDraw] = useState(false);
+    const [isChecked, setIsChecked] = useState(false);
 
     return(
-        <div className="">
+        <div className=''>
         <Header />
         <div className="w-full h-full">
-            <ReactMapGL 
-                {...viewport}
-                width="100vw" 
-                height="100vh" 
-                mapStyle="mapbox://styles/mapbox/light-v10"
-                onViewportChange={setViewport}
-                mapboxApiAccessToken={'pk.eyJ1IjoibWl0aTIxIiwiYSI6ImNrdzNoamxwdTFka2syb3JvdWRhM3EwNW8ifQ.OV5IlhtvWXgW2SwJbi_xYw'}
+            <div className='izris'>
+                {isDraw && <Parkirna />}
+            </div>
+            <div className='izris-markers'>
+                {isMarker && 
+                <ReactMapGL 
+                    {...viewport}
+                    width="100vw" 
+                    height="100vh" 
+                    mapStyle="mapbox://styles/mapbox/light-v10"
+                    onViewportChange={setViewport}
+                    mapboxApiAccessToken={'pk.eyJ1IjoibWl0aTIxIiwiYSI6ImNrdzNoamxwdTFka2syb3JvdWRhM3EwNW8ifQ.OV5IlhtvWXgW2SwJbi_xYw'}
                 >
-                
-                {parkirisca.filter((val) => {
-                    if (searchTerm == '') {
-                        return val;
+                    
+                    {parkirisca.filter((val) => {
+                        if (searchTerm == '') {
+                            return val;
+                        }
+                        else if (val.lokacija_parkirisca.toLowerCase().includes(searchTerm.toLowerCase())) {
+                            return val;
+                        }
+                        else if (val.opis_lokacije.toLowerCase().includes(searchTerm.toLowerCase())) {
+                            return val;
+                        }
+                    }).map(
+                    park => (
+                        <Marker 
+                        key={park.lokacija_parkirisca} 
+                        longitude={park.LON} 
+                        latitude={park.LAT} >
+
+                            <button className="marker-btn" onClick={(e) => {
+                            e.preventDefault();
+                            setSelectedPark(park);
+                            
+                            zoomToMarker(
+                                park.LON,
+                                park.LAT,
+                                [park.LON, park.LAT]);
+                            }}>
+
+                            <img className="marker-icon" src="icons/parking.png" alt="Marker icon"/>
+                            </button>
+
+                        </Marker>
+                    ))
                     }
-                    else if (val.lokacija_parkirisca.toLowerCase().includes(searchTerm.toLowerCase())) {
-                        return val;
-                    }
-                    else if (val.opis_lokacije.toLowerCase().includes(searchTerm.toLowerCase())) {
-                        return val;
-                    }
-                }).map(
-                  park => (
-                    <Marker 
-                      key={park.lokacija_parkirisca} 
-                      longitude={park.LON} 
-                      latitude={park.LAT} >
 
-                        <button className="marker-btn" onClick={(e) => {
-                          e.preventDefault();
-                          setSelectedPark(park);
-                          
-                          zoomToMarker(
-                            park.LON,
-                            park.LAT,
-                            [park.LON, park.LAT]);
-                        }}>
-
-                          <img className="marker-icon" src="icons/parking.png" alt="Marker icon"/>
-                        </button>
-
-                    </Marker>
-                  ))
-                }
-
-                {selectedPark? (
-                  <Popup 
-                    latitude={selectedPark.LAT}
-                    longitude={selectedPark.LON}
-                    onClose={() => {
-                      setSelectedPark(null);
-                      zoomOfMarker(
-                        selectedPark.LON,
-                        selectedPark.LAT,
-                        [selectedPark.LON, selectedPark.LAT]);
-                    }}  
-                  >
-                    <div>
-                      <h2><strong>{selectedPark.lokacija_parkirisca}</strong></h2>
-                      <p>{selectedPark.opis_lokacije}</p>
-                    </div>
-                  </Popup>
-                ) : null}
-                <NavigationControl style={navControlStyle} />
-                <FullscreenControl style={fullscreenControlStyle} />
-            </ReactMapGL>
+                    {selectedPark? (
+                    <Popup 
+                        latitude={selectedPark.LAT}
+                        longitude={selectedPark.LON}
+                        onClose={() => {
+                        setSelectedPark(null);
+                        zoomOfMarker(
+                            selectedPark.LON,
+                            selectedPark.LAT,
+                            [selectedPark.LON, selectedPark.LAT]);
+                        }}  
+                    >
+                        <div>
+                        <h2><strong>{selectedPark.lokacija_parkirisca}</strong></h2>
+                        <p>{selectedPark.opis_lokacije}</p>
+                        </div>
+                    </Popup>
+                    ) : null}
+                    <NavigationControl style={navControlStyle} />
+                    <FullscreenControl style={fullscreenControlStyle} />
+                </ReactMapGL>}
+            </div>
             {/* Sidebar */}
             <div className="w-full h-3/4 lg:-mt-96 lg:w-1/4 px-8 py-5 ml-auto rounded-md sidebar blur">
                   <div className="flex flex-col">
                       {/* SearchBar */}
-                      <input type="text" placeholder="Poišči električno polnilnico..." className="text-gray-400 search rounded-md border-0 focus:outline-none focus:ring-0 focus:border-blue-500 flex-grow p-2" onChange={event => {setSearchTerm(event.target.value)}}/>
+                      {isMarker && 
+                        <input type="text" placeholder="Poišči električno polnilnico..." className="text-gray-400 search rounded-md border-0 focus:outline-none focus:ring-0 focus:border-blue-500 flex-grow p-2" onChange={event => {setSearchTerm(event.target.value)}}/>
+                      }               
+                      <div className='showParkings'>
+                        <input 
+                            type="checkbox" 
+                            id="topping" 
+                            name="topping" 
+                            value="Parkings"  
+                            checked={isChecked}
+                            onChange={(e) => {
+                                if (isChecked) {
+                                    //history.push('/');
+                                    window.location = '/';
+                                }
+                                handleOnChange();
+                                setIsDraw(!isDraw);
+                                setIsMarker(!isMarker);
+                            }}
+                        /> Pokaži izris parkirišč
+                      </div>
                       {parkirisca.filter((val) => {
                         if (searchTerm == '') {
                             return val;
@@ -272,29 +217,7 @@ function ParkirnaMesta() {
         </div>
         );
     
-        {/*
-        <div className="">
-            <Header />
-            <header className="bg-white shadow">
-                <div className="max-w-full mx-auto py-6">
-                    <h1 className="text-3xl font-bold text-gray-900 px-4 sm:px-6 lg:px-8">Parkirna mesta</h1>
-                </div>
-            </header>
-            <div className="px-4 sm:px-6 lg:px-8 mt-8">
-                {
-                    parkirisca.map (data => 
-                        <div>
-                            <span><b>Lokacija parkirišča: </b>{data.lokacija_parkirisca}</span>
-                            <span><b> Opis lokacije: </b>{data.opis_lokacije}</span>
-                            <span><b> Parkirni prostori: </b>{data.parkirni_prostori}</span>
-                            <span><b> Parkirni prostori za invalide: </b>{data.parkirni_prostori_za_invalide}</span>
-                            <span><b> Parkirni režim: </b>{data.parkrini_rezim}</span>    
-                        </div>
-                    )
-                }
-            </div>
-        </div>
-            );*/}
+       
 }
 
 export default ParkirnaMesta
